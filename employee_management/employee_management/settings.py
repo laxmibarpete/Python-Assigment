9fbd9990f128c28158a0e944e7c22080c3ac1b65
laxmibarpete <laxmibarpete1994@gmail.com>
2018-07-05 17:28:50 +0530

    DataBase Setup

--- employee_management/employee_management/settings.py
+++ employee_management/employee_management/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'employee_management.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'employee',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
